2420ab7884d0845261099e95ebf46521140bc829
Dephie <zephyrusic@icloud.com>
2024-05-07 10:30:02 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["tsconfig.json", "vite.config.ts"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prefer-const": "warn",
+    "no-prototype-builtins": "off",
+    "no-empty": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "warn",
+  },
+};
